865487b48b1a50edfcedd35b8a9965af904e07f3
Abdullah Naveed <128359566+abdullahnaveedan@users.noreply.github.com>
2024-04-17 11:15:43 +0500

    Signup Api

--- imageapp/urls.py
+++ imageapp/urls.py
@@ -2,14 +2,14 @@ from django.urls import path
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
-
+from .views import createAccount
 urlpatterns = [
     path('', views.index, name="index"),
     path("diffuse-image/", views.diffuseImg, name="diffuseImage"),
     path("accounts/login/", views.loginPage, name="loginPage"),
     path("signin_user/", views.accountslogin, name="acccountslogin"),
     path("accounts/logout/", views.logout, name="logout"),
-    path("signup_user/", views.createAccount, name="signup"), #  API for register user
+    path("accounts/signup/", createAccount.as_view()), #  API for register user
     path("diffuse-image/submitRecord/", views.submitRecord, name="submitRecord"),
     path("download/", views.download, name="download"),
     path("language-translator/", views.language_translator, name="translator"),
